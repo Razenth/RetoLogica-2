d1edfb1affe7c41e3cab9cc71b7b3df3004ff9e8
Razenth <santiago.gonzalezagudelo@gmail.com>
2023-12-04 17:03:46 -0500

    feat: :sparkles: All exercise done

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        System.Console.WriteLine("Iniciar programa");
+        System.Console.Write("Si(Y) / No(N): ");
+        string option = Console.ReadLine().ToUpper();
+        switch (option)
+        {
+            case "Y":
+                Console.Clear();
+                System.Console.Write("Ingrese Multiplicador: ");
+                int multiplicador = int.Parse(Console.ReadLine());
+                int mayor = multiplicador;
+                int menor = 0;
+                System.Console.Write("Ingrese Multiplicando: ");
+                int multiplicando = int.Parse(Console.ReadLine());
+                if (multiplicando > multiplicador)
+                {
+                    mayor = multiplicando;
+                    menor = multiplicador;
+                }
+                else
+                {
+                    menor = multiplicando;
+                }
+                List<int> list = new List<int>();
+                bool flag = true;
+                while (flag)
+                {
+                    if (mayor == 1){
+                        list.Add(menor);
+                        flag = false;
+                    }
+                    else if(mayor % 2 != 0)
+                    {
+                        list.Add(menor);
+                    }
+                    mayor = mayor / 2;
+                    menor = menor * 2;
+                }
+                System.Console.WriteLine("Resultado: {0}",list.Sum());
+                break;
+        }
+        System.Console.WriteLine("Presione enter para terminar el programa");
+        Console.ReadLine();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # RetoLogica-2
+## Reto Multiplicación Rusa
+En este ejercicio implementé un pequeño menú para iniciar el programa
+```terminal
+Iniciar programa
+Si(Y) / No(N):  
+```
+Si la persona digita "Y", ejecutará el programa donde podrá ingresar dos números e internamente el programa se encargá de realizar la operación de Multiplicación
+
+Finalmente en la consola le mostrará el resultado de la operación 
+
+```terminal
+Ingrese Multiplicador: 37
+Ingrese Multiplicando: 12
+Resultado: 444
+```
+
+
